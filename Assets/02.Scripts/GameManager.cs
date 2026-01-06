116d3f801a5dc5c72800128cda39aad193af726b
hmssojeong <sojeong9638@gmail.com>
2026-01-06 09:42:36 +0900

    feat: GameManager 스크립트 추가

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,4 +4,14 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
     public static GameManager Instance => _instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
+    }
 }
