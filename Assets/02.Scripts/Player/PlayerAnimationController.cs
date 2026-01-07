40457c93852cd286a7391911d590e47c2e9489f6
hmssojeong <sojeong9638@gmail.com>
2026-01-07 14:42:57 +0900

    feat: 플레이어 전투 애니메이션 제어 추가

--- Assets/02.Scripts/Player/PlayerAnimationController.cs
+++ Assets/02.Scripts/Player/PlayerAnimationController.cs
@@ -2,15 +2,96 @@ using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Animation Settings")]
+    [SerializeField] private float _attackAnimationSpeed = 1f;
+
+    private Animator _animator;
+
+    private static readonly int _attackTrigger = Animator.StringToHash("Attack");
+    private static readonly int _comboStepParameters = Animator.StringToHash("ComboStep");
+    private static readonly int _isAttackingParameters = Animator.StringToHash("IsAttacking");
+    private static readonly int _damageTrigger = Animator.StringToHash("Damage");
+    private static readonly int _deathTrigger = Animator.StringToHash("Death");
+    private static readonly int _attackSpeedParameters = Animator.StringToHash("AttackSpeed");
+
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
+
+        if(_animator == null)
+        {
+            Debug.Log($"Animator 컴포넌트를 넣어주세요");
+            return;
+        }
+
+        SetAttackSpeed(_attackAnimationSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAttack(int comboStep)
     {
-        
+        if(_animator == null)
+        {
+            return;
+        }
+
+        _animator.SetInteger(_comboStepParameters, comboStep);
+        _animator.SetTrigger(_attackTrigger);
+        _animator.SetBool(_isAttackingParameters, true);
     }
+
+    public void PlayDamage() // 피격 애니메이션
+    {
+        if(_animator == null)
+        {
+            return;
+        }
+
+        _animator.SetTrigger(_damageTrigger);
+    }
+
+    public void PlayDeath()
+    {
+        if(_animator == null)
+        {
+            return;
+        }
+
+        _animator.SetTrigger(_deathTrigger);
+    }
+
+    public void EndAttack()
+    {
+        if(_animator == null)
+        {
+            return;
+        }
+        _animator.SetBool(_isAttackingParameters, false);
+    }
+
+    public void SetAttackSpeed(float speed)
+    {         if(_animator == null)
+        {
+            return;
+        }
+        _animator.SetFloat(_attackSpeedParameters, speed);
+    }
+
+    public AnimatorStateInfo GetCurrentAnimatorStateInfo(int layerIndex = 0)
+    {
+        return _animator != null ? _animator.GetCurrentAnimatorStateInfo(layerIndex) : default;
+    }
+
+    public bool IsPlayingAnimation(string stateName, int layerIndex = 0)
+    {
+        if(_animator == null)
+        {
+            return false;
+        }
+
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(layerIndex);
+        return stateInfo.IsName(stateName);
+
+    }
+
+
 }
