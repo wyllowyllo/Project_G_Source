2d3cc65f43c078fd7a6e41a509ab7462c3a82d00
hmssojeong <sojeong9638@gmail.com>
2026-01-08 17:31:11 +0900

    feat: skill UI 추가

---
+++ Assets/02.Scripts/UI/SkillSystem.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+public class SkillSystem : MonoBehaviour
+{
+    [SerializeField] private GraphicRaycaster _graphicRaycaster;
+    [SerializeField] private UISkill[] _skills;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && int.TryParse(Input.inputString, out int key) && key >= 1 && key <= _skills.Length)
+        {
+            _skills[key - 1].UseSkill();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _skills[0].UseSkill();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _skills[1].UseSkill();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _skills[2].UseSkill();
+        }
+    }
+
+}
