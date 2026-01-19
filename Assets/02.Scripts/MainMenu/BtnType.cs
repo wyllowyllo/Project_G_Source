ae87cb7fa113f798bcfb6fac4d8f4becdee5dd90
hmssojeong <sojeong9638@gmail.com>
2026-01-19 18:22:56 +0900

    refactor: 제미나이 리뷰 반영

--- Assets/02.Scripts/MainMenu/BtnType.cs
+++ Assets/02.Scripts/MainMenu/BtnType.cs
@@ -6,25 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public ButtonType CurrentType;
-    public Transform ButtonScale;
+    [SerializeField] private ButtonType _currentType;
+    [SerializeField] private Transform _buttonScale;
     private Vector3 _defaultScale;
     private float _rateScale = 1.2f;
 
     public void Start()
     {
-        _defaultScale = ButtonScale.localScale;
+        _defaultScale = _buttonScale.localScale;
     }
     public void OnBtnClick()
     {
-        switch (CurrentType)
+        switch (_currentType)
         {
             case ButtonType.Start:
                 SceneLoader.LoadScene("TownScene");
                 break;
 
             case ButtonType.End:
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
                 Application.Quit();
+#endif
                 Debug.Log("게임종료");
                 break;
         }
@@ -32,11 +36,11 @@ public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        ButtonScale.localScale = _defaultScale * _rateScale;
+        _buttonScale.localScale = _defaultScale * _rateScale;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        ButtonScale.localScale = _defaultScale;
+        _buttonScale.localScale = _defaultScale;
     }
 }
